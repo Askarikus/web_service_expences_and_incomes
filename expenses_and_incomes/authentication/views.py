74ffede0a063b38c2ae467001c1b958e00ed1aa6
Askarikus <askarikus@yandex.ru>
2022-02-13 17:07:05 +0500

    #8 video Ajax username validation

--- expenses_and_incomes/authentication/views.py
+++ expenses_and_incomes/authentication/views.py
@@ -1,5 +1,19 @@
+from django.http import JsonResponse
+from django.contrib.auth.models import User
 from django.shortcuts import render
 from django.views import View
+import json
+
+
+class UsernameValidationView(View):
+    def post(self, request):
+        data = json.loads(request.body)
+        username = data['username']
+        if not str(username).isalnum():
+            return JsonResponse({'username_error': 'username should only contain alphanumeric characters'}, status=400)
+        # if User.objects.filter(username=username).exists():
+        #     return JsonResponse({'username_error': 'sorry username in use,choose another one '}, status=409)
+        return JsonResponse({'username_valid': True}, status=200)
 
 
 class RegistrationView(View):
