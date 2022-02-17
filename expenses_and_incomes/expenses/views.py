a7c73b9b16e39609741ff98f94d51f7eb941992c
Askarikus <askarikus@yandex.ru>
2022-02-17 16:25:49 +0500

    #18 pagination

--- expenses_and_incomes/expenses/views.py
+++ expenses_and_incomes/expenses/views.py
@@ -1,9 +1,24 @@
+import json
+
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from django.core.paginator import Paginator
+from django.http import JsonResponse
 from django.shortcuts import render, redirect
 
 from .models import Category, Expense
+
+
+def search_expenses(request):
+    if request.method == 'POST':
+        search_str = json.loads(request.body).get('searchText')
+        expenses = Expense.objects.filter(
+            amount__istartswith=search_str, owner=request.user) | Expense.objects.filter(
+            date__istartswith=search_str, owner=request.user) | Expense.objects.filter(
+            description__icontains=search_str, owner=request.user) | Expense.objects.filter(
+            category__icontains=search_str, owner=request.user)
+        data = expenses.values()
+        return JsonResponse(list(data), safe=False)
 
 
 @login_required(login_url='/authentication/login')
